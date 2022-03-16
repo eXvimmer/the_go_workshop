b07d14c96afde1a986b4d179c84552357f941308
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-03-16 17:16:18 +0330

    Exercise 3.03 triggering number wraparound

---
+++ ch03/ex_3.03/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var (
+		a int8  = 125
+		b uint8 = 253
+	)
+
+	for i := 0; i < 5; i++ {
+		a++
+		b++
+
+		fmt.Println(i, ")", "int8", a, "uint8", b)
+	}
+
+}
