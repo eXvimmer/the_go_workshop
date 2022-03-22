9f7fd92007f740ba1bb462be801a6c06b049af85
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-03-22 05:18:28 +0430

    Exercise 4.16: Creating a Simple Custom Type

---
+++ ch04/ex_4.16/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+type id string
+
+func getIDs() (id, id, id) {
+	var (
+		id1 id
+		id2 id = "id1234"
+		id3 id
+	)
+
+	id3 = "id1234"
+
+	return id1, id2, id3
+}
+
+func main() {
+	id1, id2, id3 := getIDs()
+	fmt.Println("id1 == id2:     ", id1 == id2)
+	fmt.Println("id2 == id3:     ", id2 == id3)
+
+	fmt.Println(`id2 == "id1234":`, string(id2) == "id1234")
+}
