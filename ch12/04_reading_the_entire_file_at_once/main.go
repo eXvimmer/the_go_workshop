1541a6c905524ffa2d46947805c6d26ba2971788
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-04-04 11:03:18 +0430

    Use ReadAll method to read the contents of a file

--- ch12/04_reading_the_entire_file_at_once/main.go
+++ ch12/04_reading_the_entire_file_at_once/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
+	"strings"
 )
 
 func main() {
@@ -14,4 +16,31 @@ func main() {
 
 	fmt.Println("file contents:")
 	fmt.Println(string(content))
+
+	fmt.Println("\n-----------------------------------------------------")
+
+	f, err := os.Open("test.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	contents, err := ioutil.ReadAll(f)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("file contents:")
+	fmt.Println(string(contents))
+
+	fmt.Println("\n-----------------------------------------------------")
+	r := strings.NewReader("no file here")
+	c, err := ioutil.ReadAll(r)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("contents of strings.NewReader:")
+	fmt.Println(string(c))
 }
