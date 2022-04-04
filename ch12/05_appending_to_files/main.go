2cf2ddbf00d574028ec47383857c24c00952fae4
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-04-04 12:52:29 +0430

    Write to a file

--- ch12/05_appending_to_files/main.go
+++ ch12/05_appending_to_files/main.go
@@ -5,11 +5,15 @@ import (
 )
 
 func main() {
-	f, err := os.OpenFile("junk101.txt", os.O_CREATE, 0644)
+	f, err := os.OpenFile("junk101.txt", os.O_CREATE|os.O_WRONLY, 0644)
 
 	if err != nil {
 		panic(err)
 	}
 
 	defer f.Close()
+
+	if _, err := f.Write([]byte("adding stuff.\n")); err != nil {
+		panic(err)
+	}
 }
