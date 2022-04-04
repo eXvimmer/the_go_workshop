726de31a859b7d663941234f4522022180e3b9c6
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-04-04 12:55:54 +0430

    Append to a file

--- ch12/05_appending_to_files/main.go
+++ ch12/05_appending_to_files/main.go
@@ -5,7 +5,8 @@ import (
 )
 
 func main() {
-	f, err := os.OpenFile("junk101.txt", os.O_CREATE|os.O_WRONLY, 0644)
+	f, err := os.OpenFile("junk101.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+		0644)
 
 	if err != nil {
 		panic(err)
