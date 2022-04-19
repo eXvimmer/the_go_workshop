68d8d60b3adeae9ac37f2ab0ef5181073f881af0
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-04-19 19:27:26 +0430

    Reimplement Activity 15.03. Load the template to memory and use the handler method

--- ch15/act_15.03/main.go
+++ ch15/act_15.03/main.go
@@ -7,28 +7,40 @@ import (
 	"strings"
 )
 
-type Person struct {
+type Visitor struct {
 	Name string
 }
 
+type TemplateHandler struct {
+	tmp *template.Template
+}
+
+func (th TemplateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	v := Visitor{}
+	q := r.URL.Query()
+	names, ok := q["name"]
+	if ok {
+		v.Name = strings.Join(names, ", ")
+	}
+	th.tmp.Execute(w, v)
+}
+
+func NewTemplateHandler(tmpPath string) (*TemplateHandler, error) {
+	temp, err := template.ParseFiles(tmpPath)
+	if err != nil {
+		return nil, err
+	}
+
+	return &TemplateHandler{temp}, nil
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		temp, err := template.ParseFiles("./index.html")
-		if err != nil {
-			log.Fatal(err)
-		}
+	handler, err := NewTemplateHandler("./index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-		p := Person{}
-
-		q := r.URL.Query()
-		names, ok := q["name"]
-		if ok {
-			p.Name = strings.Join(names, ", ")
-			temp.Execute(w, p)
-			return
-		}
-		temp.Execute(w, nil)
-	})
+	http.Handle("/", handler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
