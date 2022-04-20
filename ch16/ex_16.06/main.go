0d202aa6fc04aa63fcd57c586a1c15700d76f348
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-04-20 07:58:24 +0430

    Exercise 16.06: Sum Numbers from Everywhere

---
+++ ch16/ex_16.06/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func push(from, to int, ch chan int) {
+	for i := from; i <= to; i++ {
+		ch <- i
+		time.Sleep(time.Microsecond)
+	}
+}
+
+func main() {
+	s1 := 0
+	ch := make(chan int, 100)
+	go push(1, 25, ch)
+	go push(26, 50, ch)
+	go push(51, 75, ch)
+	go push(76, 100, ch)
+
+	for i := 0; i < 100; i++ {
+		c := <-ch
+		log.Println(c)
+		s1 += c
+	}
+
+	log.Println(s1)
+}
