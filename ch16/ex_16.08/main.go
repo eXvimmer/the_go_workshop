099ee45c4d4ddc57deb22f9c8e4b57d4f089a7f0
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-04-21 08:23:09 +0430

    Exercise 16.08: Equally Splitting the Work between Routines

---
+++ ch16/ex_16.08/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "log"
+
+func worker(in, out chan int) {
+	sum := 0
+	for i := range in {
+		sum += i
+	}
+
+	out <- sum
+}
+
+func sum(workers, from, to int) int {
+	out := make(chan int, workers)
+	in := make(chan int, 4)
+
+	for i := 0; i < workers; i++ {
+		go worker(in, out)
+	}
+
+	for i := from; i <= to; i++ {
+		in <- i
+	}
+	close(in)
+
+	sum := 0
+	for i := 0; i < workers; i++ {
+		sum += <-out
+	}
+	close(out)
+
+	return sum
+}
+
+func main() {
+	sum := sum(100, 1, 100)
+	log.Println(sum)
+}
